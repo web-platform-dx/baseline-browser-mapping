e8462b87d2c1ea2ed656a442795c83eade492964
Tony Conway <tony.p.conway@gmail.com>
2025-04-17 15:26:33 +0100

    run refresh-static with fresh data

--- scripts/refresh-static.ts
+++ scripts/refresh-static.ts
@@ -4,54 +4,46 @@ import { readFileSync, writeFileSync } from "fs";
 
 // Write core versions
 writeFileSync(
-  './static/all_versions_array.json',
-  JSON.stringify(
-    getAllVersions()
-  ),
-  { encoding: 'utf-8' }
+  "./static/all_versions_array.json",
+  JSON.stringify(getAllVersions()),
+  { encoding: "utf-8" },
 );
 writeFileSync(
-  './static/all_versions_object.json',
-  JSON.stringify(
-    getAllVersions(
-      { outputFormat: 'object' }
-    )
-  ),
-  { encoding: 'utf-8' }
+  "./static/all_versions_object.json",
+  JSON.stringify(getAllVersions({ outputFormat: "object" })),
+  { encoding: "utf-8" },
 );
 writeFileSync(
-  './static/all_versions.csv',
-  getAllVersions(
-    { outputFormat: 'csv' }
-  ).toString(),
-  { encoding: 'utf-8' }
+  "./static/all_versions.csv",
+  getAllVersions({ outputFormat: "csv" }).toString(),
+  { encoding: "utf-8" },
 );
 
 // Write with downstream versions
 writeFileSync(
-  './static/with_downstream/all_versions_array.json',
+  "./static/with_downstream/all_versions_array.json",
   JSON.stringify(
     getAllVersions({
-      includeDownstreamBrowsers: true
-    })
+      includeDownstreamBrowsers: true,
+    }),
   ),
-  { encoding: 'utf-8' }
+  { encoding: "utf-8" },
 );
 writeFileSync(
-  './static/with_downstream/all_versions_object.json',
+  "./static/with_downstream/all_versions_object.json",
   JSON.stringify(
     getAllVersions({
-      outputFormat: 'object',
-      includeDownstreamBrowsers: true
-    })
+      outputFormat: "object",
+      includeDownstreamBrowsers: true,
+    }),
   ),
-  { encoding: 'utf-8' }
+  { encoding: "utf-8" },
 );
 writeFileSync(
-  './static/with_downstream/all_versions.csv',
+  "./static/with_downstream/all_versions.csv",
   getAllVersions({
-    outputFormat: 'csv',
-    includeDownstreamBrowsers: true
+    outputFormat: "csv",
+    includeDownstreamBrowsers: true,
   }).toString(),
-  { encoding: 'utf-8' }
+  { encoding: "utf-8" },
 );
