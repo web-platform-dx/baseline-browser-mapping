e1cff234d018fde433f261817fb7fd31869296fc
Tony Conway <tony.p.conway@gmail.com>
2025-11-29 13:06:44 +0000

    formatting fixes

--- src/index.ts
+++ src/index.ts
@@ -36,8 +36,8 @@ const checkUpdate = (targetDate: Date, lastUpdatedOverride?: number) => {
   if (targetDate > twoMonthsAgo && lastUpdatedToUse < twoMonthsAgo.getTime()) {
     console.warn(
       "[baseline-browser-mapping] The data in this module is over two months old and you are targetting a recent feature cut off date of " +
-      targetDate.toISOString().slice(0, 10) +
-      ". To ensure accurate Baseline data, please update to the latest version of this module using your package manager of choice.",
+        targetDate.toISOString().slice(0, 10) +
+        ". To ensure accurate Baseline data, please update to the latest version of this module using your package manager of choice.",
     );
     hasWarned = true;
   }
